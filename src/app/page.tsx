bd0a3b881ac246a7615bdfec3a25291d0747ac34
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-04-05 15:32:16 -0700

    Add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
-import Image from "next/image";
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type JournalEntry = {
+  timestamp: Date;
+  title: string;
+  context: string;
+};
 
 export default function Home() {
-  const variable = 5 + 5;
-  console.log(variable);
-  
+  const [journalEntries, addJournalEntry] = useState<Array<JournalEntry>>([]);
+
+  const onAddJournalEntry = () => {
+    addJournalEntry((prevState) => {
+      const dateCreated = new Date();
+      const entry = {
+        timestamp: dateCreated,
+        title: dateCreated.toUTCString(),
+        context: 'This is a test journal entry',
+      };
+
+      return [...prevState, entry];
+    });
+  };
+
+  useEffect(() => {
+    console.log('journalEntries:', journalEntries);
+  }, [journalEntries]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hello World!
-        </p>
+    <main className="flex min-h-screen flex-col  justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed left-0 top-0 flex h-full w-96 flex-col border-b border-gray-300 bg-white p-10 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+          <button onClick={onAddJournalEntry}>+</button>
+          <ul>
+            {journalEntries.map((entry) => (
+              <li key={entry.timestamp.getUTCSeconds()}>{entry.title}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
